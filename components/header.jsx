493ee67732615fd449914a8c2fea5ccae14869d5
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-06-30 21:02:11 +0530

    setup + Authentication

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <div className='fixed top-0'>
+         <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn></div>
+  )
+}
+
+export default Header
