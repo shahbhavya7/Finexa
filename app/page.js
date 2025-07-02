76db2b9f8cdf27e6637dd0e98849d3fb2a92c64c
shahbhavya7 <146073814+shahbhavya7@users.noreply.github.com>
2025-07-02 16:53:26 +0530

    Auth correct setup + logo setup + hero start

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import HeroSection from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>
-  </div>;
+  return (<div className="min-h-screen bg-white">
+    <HeroSection />
+  </div>);
 }
